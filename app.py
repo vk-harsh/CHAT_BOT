b3d8a8faeed02b3e3b91f6803f794c3f6c9c894f
Harsh Vardhan Kushwaha <harshvk13579@gmail.com>
2025-07-29 02:52:22 +0530

    Updated Streamlit

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def set_background():
             background-size: cover;
             background-repeat: no-repeat;
             background-attachment: fixed;
+            opacity: 0.3;
         }
 
         /* Remove black bars from input, text area, buttons */
